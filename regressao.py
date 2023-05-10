0b80701028d358fb9a8acd16c0c2f7bc94c84de4
brennofsr <45960177+brennofsr@users.noreply.github.com>
2023-05-10 06:53:41 -0300

    mais testes e gráfico

--- regressao.py
+++ regressao.py
@@ -1,45 +1,64 @@
-def makePrediction(s1, s2, s3, s4):
-    import pandas as pd
-    import numpy as np
-    from sklearn.preprocessing import PolynomialFeatures
-    from sklearn.linear_model import LinearRegression
-    from sklearn.metrics import mean_squared_error, r2_score
+import pandas as pd
+import csv
+import sys
+import io
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
-    # Carregar a base de dados do diabetes a partir do link
-    url = 'https://www4.stat.ncsu.edu/~boos/var.select/diabetes.tab.txt'
-    data = pd.read_csv(url, sep='\\t')
+def make_prediction(s1, s2, s3, s4):
+    file_content = '''${fileContent}'''
+    # Separar as linhas e os valores por tabulação (\t)
+    lines = file_content.strip().split('\n')
+    data_rows = [line.split('\t') for line in lines]
+
+    # Criar um dicionário com as colunas e valores
+    data_dict = {}
+    header = data_rows[0]  # Primeira linha contém os nomes das colunas
+    for col in header:
+        data_dict[col] = []
+
+    for row in data_rows[1:]:  # Linhas subsequentes contêm os valores
+        for i, value in enumerate(row):
+            col_name = header[i]
+            data_dict[col_name].append(value)
+
+    # Criar o DataFrame a partir do dicionário
+    data = pd.DataFrame(data_dict)
+
+    data = np.genfromtxt('diabetes.txt', delimiter='\t', skip_header=1)
+
+    # Separar os dados em atributos (X) e rótulos (Y)
+    X = data[:, :-1]
+    Y = data[:, -1]
+
+    # Converter as colunas numéricas para o tipo adequado (opcional)
+    data[['AGE', 'SEX', 'BMI', 'BP', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'Y']] = \
+        data[['AGE', 'SEX', 'BMI', 'BP', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'Y']].astype(float)
 
     # Identificar as variáveis relevantes para a progressão da diabetes
-    X = data[['BMI', 'BP', 'S4', 'S5']]  # Variáveis independentes
+    X = data[['AGE', 'SEX', 'BMI', 'BP']]  # Variáveis independentes
     y = data['Y']  # Variável dependente
 
-    # Dividir os dados em conjunto de treinamento e teste
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Separar as features (X) e o target (y)
+    # X = data[['Age', 'Sex', 'BMI', 'BP']].values
+    # y = data['Y'].values
 
-    # Criar um modelo de regressão linear
+    # Dividir os dados em treino e teste
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Criar o modelo de regressão linear
     model = LinearRegression()
+    # Treinar o modelo
+    model.fit(X_train, y_train)
 
-    # Pré-processamento dos dados de entrada
-    regr_quad = PolynomialFeatures(degree=2, include_bias=False)
-    X_train_poly = regr_quad.fit_transform(X_train)
-    X_test_poly = regr_quad.transform(X_test)
+    # Fazer previsões
+    y_pred = model.predict([s1, s2, s3, s4])
 
-    # Treinar o modelo usando o conjunto de treinamento
-    model.fit(X_train_poly, y_train)
-
-    # Fazer previsões usando o conjunto de teste
-    y_pred = model.predict(X_test_poly)
-
-    # Avaliar o desempenho do modelo
-
-    # RMSE (Raiz quadrada do erro médio quadrático)
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
-    # R^2 (Coeficiente de determinação)
+    # Calcular métricas
+    mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
 
-    # Fazer a previsão da progressão da diabetes usando o modelo treinado
-    input_data = [[s1, s2, s3, s4]]
-    input_data_poly = regr_quad.transform(input_data)
-    predicted_progression = model.predict(input_data_poly)[0]
-
-    return rmse, r2, predicted_progression
+    # Retornar o resultado
+    (y_pred[0], mse, r2)
